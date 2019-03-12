d196bcab9318ea3ce1aadcf03716080aa374807e
SangYuhiter <sangyu.code@gmail.com>
2019-03-12 13:08:52 +0800

    解决日志系统重复输出日志问题（创建时检查logger的handler列表）

--- Log/Logger.py
+++ Log/Logger.py
@@ -18,28 +18,31 @@ class MyLog:
     def __init__(self, logger):
         # 创建一个logger
         self.logger = logging.getLogger(logger)
-        self.logger.setLevel(logging.DEBUG)
 
-        # all.log文件中记录所有的日志信息，日志格式为：日期和时间 - 日志名 - 日志级别 - 日志信息
-        rf_handler = logging.handlers.TimedRotatingFileHandler(log_path + 'all.log', when='midnight', interval=1,
-                                                               backupCount=7,encoding="utf-8",
-                                                               atTime=datetime.time(0, 0, 0, 0))
-        rf_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+        # 检查当前logger是否存在，若存在，直接调用即可，不必再次创建
+        if not self.logger.handlers:
+            self.logger.setLevel(logging.DEBUG)
 
-        # error.log文件中单独记录error及以上级别的日志信息，日志格式为：日期和时间 - 日志级别 - 文件名[:行号] - 日志信息
-        f_handler = logging.FileHandler(log_path + 'error.log',encoding="utf-8")
-        f_handler.setLevel(logging.ERROR)
-        f_handler.setFormatter(
-            logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"))
+            # all.log文件中记录所有的日志信息，日志格式为：日期和时间 - 日志名 - 日志级别 - 日志信息
+            rf_handler = logging.handlers.TimedRotatingFileHandler(log_path + 'all.log', when='midnight', interval=1,
+                                                                   backupCount=7, encoding="utf-8",
+                                                                   atTime=datetime.time(0, 0, 0, 0))
+            rf_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
 
-        # 输出到控制台
-        c_handler = logging.StreamHandler()
-        c_handler.setLevel(logging.DEBUG)
-        c_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+            # error.log文件中单独记录error及以上级别的日志信息，日志格式为：日期和时间 - 日志级别 - 文件名[:行号] - 日志信息
+            f_handler = logging.FileHandler(log_path + 'error.log', encoding="utf-8")
+            f_handler.setLevel(logging.ERROR)
+            f_handler.setFormatter(
+                logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"))
 
-        self.logger.addHandler(rf_handler)
-        self.logger.addHandler(f_handler)
-        self.logger.addHandler(c_handler)
+            # 输出到控制台
+            c_handler = logging.StreamHandler()
+            c_handler.setLevel(logging.DEBUG)
+            c_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+
+            self.logger.addHandler(rf_handler)
+            self.logger.addHandler(f_handler)
+            self.logger.addHandler(c_handler)
 
     def getlog(self):
         return self.logger
