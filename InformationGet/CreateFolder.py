b5f0183beb8b26002ce2ddd58133827d9b983a53
SangYuhiter <sangyu.code@gmail.com>
2019-03-13 13:39:40 +0800

    CreateFolder.py代码规范调整+测试根目录调整为正式根目录

--- InformationGet/CreateFolder.py
+++ InformationGet/CreateFolder.py
@@ -4,7 +4,7 @@
 @File  : CreateFolder.py
 @Author: SangYu
 @Date  : 2018/12/21 11:02
-@Desc  : 完成创建文件夹的功能
+@Desc  : 完成创建文件夹的功能（工程项目开始时的操作）
 '''
 import os
 import sys
@@ -17,9 +17,9 @@ def create_plan_score_folder_c9():
     # C9及其医学部
     c9 = ["北京大学", "清华大学", "复旦大学", "上海交通大学", "浙江大学",
           "南京大学", "中国科学技术大学", "哈尔滨工业大学", "西安交通大学",
-          "北京大学医学部","上海交通大学医学部","复旦大学上海医学部"]
+          "北京大学医学部", "上海交通大学医学部", "复旦大学上海医学部"]
     catalog = ["招生计划", "录取分数"]
-    root_path = "Information/九校"
+    root_path = "Information/九校联盟"
     for university in c9:
         mylogger.info("创建%s的文件夹" % university)
         if not os.path.exists(root_path + "/" + university):
@@ -28,13 +28,13 @@ def create_plan_score_folder_c9():
                 if not os.path.exists(root_path + "/" + university + "/" + cat):
                     os.makedirs(root_path + "/" + university + "/" + cat)
                     # 创建source文件夹（存储网络爬取的原始数据）
-                    if not os.path.exists(root_path + "/" + university + "/" + cat+"/source"):
-                        os.makedirs(root_path + "/" + university + "/" + cat+"/source")
+                    if not os.path.exists(root_path + "/" + university + "/" + cat + "/source"):
+                        os.makedirs(root_path + "/" + university + "/" + cat + "/source")
         mylogger.info("%s的文件夹创建完成！" % university)
 
 
 if __name__ == "__main__":
-    mylogger = MyLog(logger=sys._getframe().f_code.co_name).getlog()
+    mylogger = MyLog(logger=__name__).getlog()
     mylogger.info("start...")
     create_plan_score_folder_c9()
     mylogger.info("end...")
