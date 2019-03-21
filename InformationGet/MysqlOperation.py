8d2bd8c0899655d15eaa90cfd15753823a1dd170
SangYuhiter <sangyu.code@gmail.com>
2019-03-21 21:44:31 +0800

    mysql查询结果优化，返回键值对应的记录列表

--- InformationGet/MysqlOperation.py
+++ InformationGet/MysqlOperation.py
@@ -1,21 +1,13 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''
+"""
 @File  : MysqlOperation.py
 @Author: SangYu
 @Date  : 2018/12/21 11:22
 @Desc  : Mysql连接、数据表创建、增删查改
-'''
-
+"""
 import mysql.connector
 import sys
-import logging
-
-
-# 日志格式设置
-LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
-logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+from Log.Logger import MyLog
 
 
 # 数据库连接（事先不确定数据库）
@@ -29,7 +21,12 @@ def connect_mysql_without_db():
 
 
 # 数据库连接(已知数据库名)
-def connect_mysql_with_db(db_name):
+def connect_mysql_with_db(db_name: str)->mysql.connector.connect:
+    """
+    数据库连接(已知数据库名)
+    :param db_name: 数据库名
+    :return: 数据库连接
+    """
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -40,46 +37,61 @@ def connect_mysql_with_db(db_name):
 
 
 # 创建数据库university_admission
-def create_database(db_name):
-    logger = logging.getLogger(sys._getframe().f_code.co_name)
+# noinspection PyProtectedMember
+def create_database(db_name: str):
+    """
+    创建数据库university_admission
+    :param db_name: 数据库名
+    :return:
+    """
+    function_logger = MyLog(logger=sys._getframe().f_code.co_name).getlog()
     mydb = connect_mysql_without_db()
     mycursor = mydb.cursor()
     mycursor.execute("SHOW DATABASES")
     dbs = []
-    logger.debug("数据库如下：")
+    function_logger.debug("数据库如下：")
     for db in mycursor:
         dbs.append(db[0])
-        logger.debug(db[0])
+        function_logger.debug(db[0])
     if db_name in dbs:
-        logger.info("数据库"+db_name+"已存在!")
+        function_logger.info("数据库" + db_name + "已存在!")
     else:
         mycursor.execute("CREATE DATABASE " + db_name)
-        logger.info(db_name+"已创建!")
+        function_logger.info(db_name + "已创建!")
 
 
 # 查询数据库中表名
-def search_table_in_db(db_name):
+# noinspection PyProtectedMember
+def search_table_in_db(db_name: str)->list:
+    """
+    查询数据库中表名
+    :param db_name: 数据库名
+    :return: 数据库中表名列表
+    """
+    function_logger = MyLog(logger=sys._getframe().f_code.co_name).getlog()
     mydb = connect_mysql_with_db(db_name)
     mycursor = mydb.cursor()
     mycursor.execute("SHOW TABLES")
     tables = []
-    logger.debug(db_name+"数据库中有以下表：")
+    function_logger.debug(db_name + "数据库中有以下表：")
     for table in mycursor:
         tables.append(table[0])
-        logger.debug(table[0])
+        function_logger.debug(table[0])
     return tables
 
 
 # 创建招生计划表
+# noinspection PyProtectedMember,SqlResolve
 def create_admission_plan_table():
+    function_logger = MyLog(logger=sys._getframe().f_code.co_name).getlog()
     db_name = "university_admission"
     tables = search_table_in_db(db_name)
     mydb = connect_mysql_with_db(db_name)
     mycursor = mydb.cursor()
 
     if "admission_plan" in tables:
-        logger.info("admission_plan表已存在！")
-        logger.info("正在删除admission_plan表...")
+        function_logger.info("admission_plan表已存在！")
+        function_logger.info("正在删除admission_plan表...")
         mycursor.execute("DROP TABLE admission_plan;")
 
     mycursor.execute("CREATE TABLE admission_plan("
@@ -90,18 +102,20 @@ def create_admission_plan_table():
                      "major VARCHAR(100),"
                      "classy varchar(10),"
                      "numbers varchar(10))")
-    logger.info("admission_plan表已重新创建！")
+    function_logger.info("admission_plan表已重新创建！")
 
 
 # 创建录取分数表（各省）
+# noinspection PyProtectedMember,SqlResolve
 def create_admission_score_pro_table():
+    function_logger = MyLog(logger=sys._getframe().f_code.co_name).getlog()
     db_name = "university_admission"
     tables = search_table_in_db(db_name)
     mydb = connect_mysql_with_db(db_name)
     mycursor = mydb.cursor()
     if "admission_score_pro" in tables:
-        logger.info("admission_score_pro表已存在！")
-        logger.info("正在删除admission_score_pro表...")
+        function_logger.info("admission_score_pro表已存在！")
+        function_logger.info("正在删除admission_score_pro表...")
         mycursor.execute("DROP TABLE admission_score_pro;")
     mydb = connect_mysql_with_db(db_name)
     mycursor = mydb.cursor()
@@ -114,18 +128,20 @@ def create_admission_score_pro_table():
                      "batch varchar(30),"
                      "classy varchar(10),"
                      "line varchar(30))")
-    logger.info("admission_score_pro表创建完成！")
+    function_logger.info("admission_score_pro表创建完成！")
 
 
 # 创建录取分数表（各专业）
+# noinspection PyProtectedMember,SqlResolve
 def create_admission_score_major_table():
+    function_logger = MyLog(logger=sys._getframe().f_code.co_name).getlog()
     db_name = "university_admission"
     tables = search_table_in_db(db_name)
     mydb = connect_mysql_with_db(db_name)
     mycursor = mydb.cursor()
     if "admission_score_major" in tables:
-        logger.info("admission_score_major表已存在！")
-        logger.info("正在删除admission_score_major表...")
+        function_logger.info("admission_score_major表已存在！")
+        function_logger.info("正在删除admission_score_major表...")
         mycursor.execute("DROP TABLE admission_score_major;")
 
     # 高校的专业分数线(学校、地区、年份、专业、类别、最高分、平均分、最低分、录取人数)
@@ -140,25 +156,60 @@ def create_admission_score_major_table():
                      "average varchar(10) NULL,"
                      "lowest varchar(10),"
                      "amount varchar(10) NULL)")
-    logger.info("admission_score_major表创建完成！")
+    function_logger.info("admission_score_major表创建完成！")
 
 
 # mysql 查询语句,返回查询结果
-def mysql_query_sentence(mysql_string):
+def mysql_query_sentence(mysql_string: str)->list:
+    """
+    mysql 查询语句,返回查询结果,记录键值列表
+    :param mysql_string: MySQL查询语句
+    :return: 记录键值列表
+    """
     dbname = "university_admission"
     mydb = connect_mysql_with_db(dbname)
     mycursor = mydb.cursor()
     mycursor.execute(mysql_string)
-    myresult = mycursor.fetchall()
+    des = mycursor.description
+    column_name = [column[0] for column in des]
+    tempresult = mycursor.fetchall()
+    myresult = []
+    for record in tempresult:
+        record_dict = {}
+        for column, word in zip(column_name, record):
+            record_dict[column] = word
+        myresult.append(record_dict)
     return myresult
 
+
+# 查询指定表的表头
+def query_table_head(table_name: str)->list:
+    """
+    查询指定表的表头
+    :param table_name: 表名
+    :return: 表头列表
+    """
+    dbname = "university_admission"
+    mydb = connect_mysql_with_db(dbname)
+    mycursor = mydb.cursor()
+    mysql_string = "select id from " + table_name + "where id ='1';"
+    mycursor.execute(mysql_string)
+    des = mycursor.description
+    column_name = [column[0] for column in des]
+    return column_name
+
+
 if __name__ == "__main__":
-    logger = logging.getLogger(__name__)
-    logger.info("begin...")
-    db_name = "university_admission"
+    main_logger = MyLog(logger=__name__).getlog()
+    main_logger.info("begin...")
+    # db_name = "university_admission"
     # create_database(db_name)
     # search_table_in_db(db_name)
-    create_admission_plan_table()
-    create_admission_score_pro_table()
-    create_admission_score_major_table()
-    logger.info("end...")
+    # create_admission_plan_table()
+    # create_admission_score_pro_table()
+    # create_admission_score_major_table()
+    # noinspection SqlResolve
+    test_myresult = mysql_query_sentence("select * from admission_plan where id='1';")
+    for item in test_myresult:
+        print(item)
+    main_logger.info("end...")
