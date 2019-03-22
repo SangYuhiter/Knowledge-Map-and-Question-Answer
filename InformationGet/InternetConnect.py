52b19614e2cc5e525d292fafbde1152e0c8dc83f
SangYuhiter <sangyu.code@gmail.com>
2019-03-22 09:38:13 +0800

    关闭requests持久连接

--- InformationGet/InternetConnect.py
+++ InformationGet/InternetConnect.py
@@ -105,9 +105,9 @@ def get_headers():
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
         "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
     ]
-    user_agent_list = user_agent_list_1 + user_agent_list_2 + user_agent_list_3;
+    user_agent_list = user_agent_list_1 + user_agent_list_2 + user_agent_list_3
     UserAgent = random.choice(user_agent_list)
-    headers = {'User-Agent': UserAgent}
+    headers = {'User-Agent': UserAgent, "Connection": "close"}
     return headers
 
 
