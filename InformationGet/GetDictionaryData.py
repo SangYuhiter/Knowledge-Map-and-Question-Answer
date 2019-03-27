7f2670a7eb006bf9b50234e8d21dbd9f2556e6d5
SangYuhiter <sangyu.code@gmail.com>
2019-03-27 15:52:01 +0800

    语法规范调整

--- InformationGet/GetDictionaryData.py
+++ InformationGet/GetDictionaryData.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 @File  : GetDictionaryData.py
 @Author: SangYu
 @Date  : 2019/2/25 23:39
 @Desc  : 从数据库、网络资源中获取词典信息
-'''
+"""
 from InformationGet.MysqlOperation import mysql_query_sentence
 import re
 import sys
@@ -16,9 +16,10 @@ dictionary_path = "../venv/Lib/site-packages/pyhanlp/static/data/dictionary/grad
 
 
 # 学校词典（全称和简称）
+# noinspection PyProtectedMember
 def build_school_dict():
-    mylogger = MyLog(logger=sys._getframe().f_code.co_name).getlog()
-    mylogger.info("构造学校名称词典...")
+    function_logger = MyLog(logger=sys._getframe().f_code.co_name).getlog()
+    function_logger.info("构造学校名称词典...")
     c9 = ["北京大学", "清华大学", "复旦大学", "上海交通大学", "浙江大学",
           "南京大学", "中国科学技术大学", "哈尔滨工业大学", "西安交通大学",
           "北京大学医学部", "上海交通大学医学部", "复旦大学上海医学部"]
@@ -31,47 +32,48 @@ def build_school_dict():
             school_dict.write(item + "\n")
         for item in c9_j:
             school_dict.write(item + "\n")
-    mylogger.info("构造学校名称词典完成！")
+    function_logger.info("构造学校名称词典完成！")
 
 
 # 专业词典，从mysql表中获取，以提高问句中专业的识别率
+# noinspection PyProtectedMember,SqlResolve
 def build_mysql_major_dict():
-    mylogger = MyLog(logger=sys._getframe().f_code.co_name).getlog()
-    mylogger.info("获取招生计划表中的专业字段...")
+    function_logger = MyLog(logger=sys._getframe().f_code.co_name).getlog()
+    function_logger.info("获取招生计划表中的专业字段...")
     # 招生计划中的专业名称
     plan_sql_string = "SELECT major FROM admission_plan GROUP BY major;"
     myresult = mysql_query_sentence(plan_sql_string)
-    mylogger.debug("招生计划表中专业数%d:" % len(myresult))
-    pattern = re.compile(r"[\(\（\[].*?[\)\）\]].*")
+    function_logger.debug("招生计划表中专业数%d:" % len(myresult))
+    pattern = re.compile(r"[(（[].*?[)）\]].*")
     plan_major_set = set()
     for major in myresult:
         temp = re.sub(pattern, "", major[0])
         plan_major_set.add(temp)
-    mylogger.debug("招生计划表中专业数(统计合并后):%d" % len(plan_major_set))
-    mylogger.debug(str(sorted(list(plan_major_set), key=lambda x: lazy_pinyin(x.lower())[0][0])))
+    function_logger.debug("招生计划表中专业数(统计合并后):%d" % len(plan_major_set))
+    function_logger.debug(str(sorted(list(plan_major_set), key=lambda x: lazy_pinyin(x.lower())[0][0])))
 
     # 录取分数中的专业名称
     score_sql_string = "SELECT major FROM admission_score_major GROUP BY major;"
     myresult = mysql_query_sentence(score_sql_string)
-    mylogger.debug("录取分数表中专业数%d:" % len(myresult))
+    function_logger.debug("录取分数表中专业数%d:" % len(myresult))
     score_major_set = set()
     for major in myresult:
         temp = re.sub(pattern, "", major[0])
         score_major_set.add(temp)
-    mylogger.debug("录取分数表中专业数(统计合并后):%d" % len(score_major_set))
-    mylogger.debug(str(sorted(list(score_major_set), key=lambda x: lazy_pinyin(x.lower())[0][0])))
+    function_logger.debug("录取分数表中专业数(统计合并后):%d" % len(score_major_set))
+    function_logger.debug(str(sorted(list(score_major_set), key=lambda x: lazy_pinyin(x.lower())[0][0])))
 
     # 测定两者交集
-    mylogger.debug("以上两者的交集为：")
+    function_logger.debug("以上两者的交集为：")
     major_and_set = plan_major_set.intersection(score_major_set)
-    mylogger.debug("交集长度为：%d", len(major_and_set))
-    mylogger.debug(str(sorted(list(major_and_set), key=lambda x: lazy_pinyin(x.lower())[0][0])))
+    function_logger.debug("交集长度为：%d", len(major_and_set))
+    function_logger.debug(str(sorted(list(major_and_set), key=lambda x: lazy_pinyin(x.lower())[0][0])))
 
     # 测定两者并集
-    mylogger.debug("以上两者的并集为：")
+    function_logger.debug("以上两者的并集为：")
     major_or_set = plan_major_set.union(score_major_set)
-    mylogger.debug("并集长度为：%d", len(major_or_set))
-    mylogger.debug(str(sorted(list(major_or_set), key=lambda x: lazy_pinyin(x.lower())[0][0])))
+    function_logger.debug("并集长度为：%d", len(major_or_set))
+    function_logger.debug(str(sorted(list(major_or_set), key=lambda x: lazy_pinyin(x.lower())[0][0])))
 
     # 根据资料构建词典
     # with open(dictionary_path + "/major.txt", "w", encoding="utf-8") as major_dict:
@@ -81,9 +83,10 @@ def build_mysql_major_dict():
 
 
 # 百度百科获取大学专业目录
+# noinspection PyProtectedMember
 def build_university_major_dict():
-    mylogger = MyLog(logger=sys._getframe().f_code.co_name).getlog()
-    mylogger.info("构造专业名称词典...")
+    function_logger = MyLog(logger=sys._getframe().f_code.co_name).getlog()
+    function_logger.info("构造专业名称词典...")
     source_path = "Information/大学/大学学科(百度百科网页源码).txt"
     with open(source_path, "r", encoding="utf-8") as source_file:
         main_page_source = source_file.read()
@@ -111,26 +114,27 @@ def build_university_major_dict():
         major_dict.truncate()
         for item in result:
             major_dict.write(re.findall(r'[\u4e00-\u9fa5]+', item)[0] + "\n")
-    mylogger.info("构造专业名称词典完成")
+    function_logger.info("构造专业名称词典完成")
 
 
 # 类别词典
+# noinspection PyProtectedMember
 def build_classy_dict():
-    mylogger = MyLog(logger=sys._getframe().f_code.co_name).getlog()
-    mylogger.info("构造类别名称词典...")
+    function_logger = MyLog(logger=sys._getframe().f_code.co_name).getlog()
+    function_logger.info("构造类别名称词典...")
     classy = ["文科", "理科", "文史", "理工"]
     with open(dictionary_path + "/classy.txt", "w", encoding="utf-8") as classy_dict:
         classy_dict.truncate()
         for item in classy:
             classy_dict.write(item + "\n")
-    mylogger.info("构造类别名称词典完成！")
+    function_logger.info("构造类别名称词典完成！")
 
 
 if __name__ == '__main__':
-    mylogger = MyLog(logger=__name__).getlog()
-    mylogger.info("start...")
+    main_logger = MyLog(logger=__name__).getlog()
+    main_logger.info("start...")
     # build_school_dict()
     # build_mysql_major_dict()
     # build_university_major_dict()
     build_classy_dict()
-    mylogger.info("end...")
+    main_logger.info("end...")
