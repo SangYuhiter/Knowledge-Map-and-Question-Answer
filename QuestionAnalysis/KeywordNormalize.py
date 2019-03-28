e5fbb016b7fd78b83206eb3b67f69b18a0fbf93b
SangYuhiter <sangyu.code@gmail.com>
2019-03-28 15:25:40 +0800

    加入本地实现的时间词识别接口

--- QuestionAnalysis/KeywordNormalize.py
+++ QuestionAnalysis/KeywordNormalize.py
@@ -8,6 +8,7 @@
 from Log.Logger import MyLog
 import json
 import requests
+from QuestionAnalysis.TimeNER import text_to_year
 
 
 # input: string
@@ -39,26 +40,32 @@ import requests
 # {"type": "timespan", "timespan": ["2018-02-16 00:00:00", "2018-03-02 00:00:00"]}
 # 时间词正则化，返回20xx(年)
 # noinspection PyDictCreation
-def time_word_normalize(time_word):
+def time_word_normalize_web(time_word):
     src = time_word
     # 外部访问url
     url_web = 'http://api.deepintell.net/timeanlz'
     # 内网访问
-    # url = "http://192.168.100.202:20002"
+    url = "http://192.168.100.202:20002"
     param = {}
     param['query'] = src
     # 外部访问头
     headers_web = {'X-Token': 'hqcK70wBTd2vqsI18JgUFUQXbbCp5JdL', 'Content-type': 'application/json',
                    'Accept': 'application/json'}
-    # headers = {'content-type': 'application/json', 'Accept': 'application/json'}
+    headers = {'content-type': 'application/json', 'Accept': 'application/json'}
     r = requests.post(url=url_web, data=json.dumps(param), headers=headers_web, timeout=3)
     # 两个接口返回参数不同，注意区别
     # time_word_dict = json.loads(r.json())
     time_word_dict = r.json()
+    print(time_word_dict)
     year = ""
     if "timestamp" in time_word_dict:
         year = time_word_dict["timestamp"][:4]
     return year
+
+
+# 时间词正则化(本地实现)
+def time_word_normalize_local(text: str)->list:
+    return text_to_year(text)
 
 
 # 返回结果为json字典列表(如下)：当有多个地理信息出现时识别出多个信息, 各信息以列表的形式返回
@@ -88,15 +95,15 @@ def district_word_normalize(district_word):
     # 外部访问
     url_web = 'http://api.deepintell.net/locanlz'
     # 内网访问
-    # url = "http://192.168.100.202:20001"
+    url = "http://192.168.100.202:20001"
     param = {}
     param['query'] = src
     # 在http://deepintell.net上注册用户获取api密钥
     headers_web = {'X-Token': 'hqcK70wBTd2vqsI18JgUFUQXbbCp5JdL', 'Content-type': 'application/json',
                    'Accept': 'application/json'}
-    # headers = {'content-type': 'application/json', 'Accept': 'application/json'}
+    headers = {'content-type': 'application/json', 'Accept': 'application/json'}
     r = requests.post(url=url_web, data=json.dumps(param), headers=headers_web, timeout=3)
-    # print(r.json())
+    print(r.json())
     district = ""
     if len(r.json()) != 0:
         district_word_dict = r.json()[0]
@@ -111,9 +118,8 @@ def district_word_normalize(district_word):
 if __name__ == '__main__':
     main_logger = MyLog(logger=__name__).getlog()
     main_logger.info("start...")
-    test_time_word = "我要去金华买火腿, 我该怎么走?"
-    test_district_word = "今年农历四月初五"
-    # print(time_word_normalize(test_time_word))
-    r_district = district_word_normalize(test_district_word)
-    print(r_district)
+    test_time_word = "今年农历四月初五"
+    test_district_word = "我要去金华买火腿, 我该怎么走?"
+    print(time_word_normalize_web(test_time_word))
+    print(district_word_normalize(test_district_word))
     main_logger.info("end...")
