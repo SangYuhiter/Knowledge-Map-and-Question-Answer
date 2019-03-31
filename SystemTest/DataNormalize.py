d5d4382ae825e6d4baa61642194336d657dddbcf
SangYuhiter <sangyu.code@gmail.com>
2019-03-31 20:40:57 +0800

    使用pickle存储常问问题集，每个问题是一个字典，五个键值对（标题，来源，时间，问题，答案）

--- SystemTest/DataNormalize.py
+++ SystemTest/DataNormalize.py
@@ -9,10 +9,11 @@ from FileRead.FileNameRead import read_all_file_list
 from Log.Logger import MyLog
 import sys
 import csv
+import pickle
 
 
 # 处理常用问题集(csv),问题和答案部分
-# noinspection PyProtectedMember
+# noinspection PyProtectedMember,PyShadowingNames,PyDictCreation
 def frequent_question_normalize(dir_path: str):
     """
     处理常用问题集(csv),问题和答案部分
@@ -21,35 +22,28 @@ def frequent_question_normalize(dir_path: str):
     """
     function_logger = MyLog(logger=sys._getframe().f_code.co_name).getlog()
     function_logger.info("开始进行数据处理...")
-    file_list = read_all_file_list(dir_path)
+    file_list = read_all_file_list(dir_path + "/source")
     for file in file_list:
         function_logger.debug(file)
         school_name = file.split("\\")[-1][:-9]
         function_logger.info("开始读取%s的常问问题集..." % school_name)
         with open(file, "r", encoding="utf-8") as csvfile:
             csv_reader = csv.reader(csvfile)
-            table_lines = []
+            fqa_lines = []
             for row in csv_reader:
-                table_lines.append(row)
-        table_head = table_lines[0]
-        table_content = table_lines[1:]
-        table_content_new = []
-        function_logger.debug(str(table_head))
-        for i_line in range(len(table_content)):
-            if len(table_content[i_line]) == 5:
-                table_content[i_line][-1] = table_content[i_line][-1] \
-                    .replace("\ue63c", "").replace("\u3000", "").replace("\n", "，").replace(" ", "").lstrip("，")
-                table_content[i_line][-2] = table_content[i_line][-2] \
-                    .replace("\u3000", "").replace("\n", "，").replace(" ", "")
-                table_content_new.append(table_content[i_line])
-                # function_logger.debug(str(table_content[i_line]))
+                if len(row) == 5:
+                    line = {}
+                    line["title"] = row[0].replace(" ", "")
+                    line["from"] = row[1]
+                    line["time"] = row[2]
+                    line["question"] = row[3].replace("\u3000", "").replace("\n", "，").replace(" ", "")
+                    line["answer"] = row[4].replace("\ue63c", "").replace("\u3000", "").replace("\n", "，")\
+                        .replace(" ", "").lstrip("，")
+                    fqa_lines.append(line)
         function_logger.info("读取%s的常用问题集完成！" % school_name)
         function_logger.info("开始写入%s的常用问题集..." % school_name)
-        with open(dir_path + "/预处理/" + school_name + "常用问题集(预处理).csv", "w", newline='', encoding='utf-8') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(table_head)
-            for line in table_content_new:
-                writer.writerow(line)
+        with open(dir_path + "/预处理/pickle/" + school_name, "wb")as p_file:
+            pickle.dump(fqa_lines, p_file)
         function_logger.info("写入%s的常用问题集完成！" % school_name)
     function_logger.info("数据处理完成！")
 
@@ -59,4 +53,8 @@ if __name__ == '__main__':
     main_logger.info("start...")
     question_set_dir = "../InformationGet/Information/大学/常问问题集"
     frequent_question_normalize(question_set_dir)
+    # with open(question_set_dir + "/预处理/pickle/" + "上海交通大学医学院", "rb")as p_file:
+    #     data = pickle.load(p_file)
+    # for q in data:
+    #     print(q)
     main_logger.info("end...")
