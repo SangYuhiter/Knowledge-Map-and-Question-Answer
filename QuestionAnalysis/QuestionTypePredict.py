4a874fa2e065c01114a7fa70bf6b1b7851f21004
SangYuhiter <sangyu.code@gmail.com>
2019-04-30 09:52:56 +0800

    增加使用关键词（如招生）和使用模板（如今年xx招生多少人）对问题类型进行判断

--- QuestionAnalysis/QuestionTypePredict.py
+++ QuestionAnalysis/QuestionTypePredict.py
@@ -9,11 +9,12 @@ import jieba
 import fastText.FastText as ff
 import time
 import os
+from SimilarityCalculate.SentenceSimilartity import edit_distance
 
 
-class QuestionTypePredict:
+class QTPredictModel:
     """
-    问题类型预测类，用于预测问题类型，规定接口可进行替换
+    使用模型对问题类型预测，用于预测问题类型，规定接口可进行替换
     """
     label_to_name = None
     stop_words = None
@@ -84,13 +85,72 @@ class QuestionTypePredict:
         return self.label_to_name[predict[0][0]]
 
 
+class QTPredictKeyword:
+    """
+    使用问句中的关键词对问句类型进行判断
+    """
+    keywords_label_map = {}
+
+    def pre_load_keyword(self):
+        """
+        预加载关键词
+        :return:
+        """
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+        with open("question_type_keyword", "r", encoding="utf-8") as f_keywords:
+            for line in f_keywords:
+                split_line = line.strip().split(" ")
+                for word in split_line[1:]:
+                    self.keywords_label_map[word] = split_line[0]
+
+    def question_predict_by_keyword(self, question):
+        for word in jieba.cut(question):
+            if word in self.keywords_label_map:
+                return self.keywords_label_map[word]
+
+
+class QTPredictTemplate:
+    """
+    使用问题模板对问句类型进行判断
+    """
+    question_template_label_map={}
+    def pre_load_question_template(self):
+        """
+        预加载问题模板
+        :return:
+        """
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+        with open("question_type_template", "r", encoding="utf-8") as f_template:
+            for line in f_template:
+                split_line = line.strip().split(" ")
+                for template in split_line[1:]:
+                    self.question_template_label_map[template]=split_line[0]
+
+    def question_predict_by_template(self,question):
+        # 相似度计算
+        score_list = edit_distance(question,list(self.question_template_label_map.keys()))
+        print(score_list[0])
+        if score_list[0][0]<0.5:
+            return self.question_template_label_map[score_list[0][2]]
+        else:
+            return ""
+
+
 if __name__ == '__main__':
     start_time = time.time()
-    qt_predict = QuestionTypePredict()
+    qt_predict = QTPredictModel()
     qt_predict.pre_load_jieba()
     qt_predict.pre_load_label_name_map()
     qt_predict.pre_load_stop_words()
     qt_predict.pre_load_fastText_model()
     print("系统数据加载时间%s" % (time.time() - start_time))
-    label = qt_predict.question_predict_by_fastText("我是一个男生，请老师推荐一下比较好的专业")
+    # label = qt_predict.question_predict_by_fastText("我是一个男生，请老师推荐一下比较好的专业")
+    # print(label)
+    # qt_predict_keyword = QTPredictKeyword()
+    # qt_predict_keyword.pre_load_keyword()
+    # label = qt_predict_keyword.question_predict_by_keyword("哈工大2017年在河北招多少人？")
+    # print(label)
+    qt_predict_template = QTPredictTemplate()
+    qt_predict_template.pre_load_question_template()
+    label = qt_predict_template.question_predict_by_template("今年哈工大招多少人？")
     print(label)
